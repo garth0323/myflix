64ae9599f43dfafa3f33f5af6612eee8290e0050
Garth Gaughan <garth0323@gmail.com>
2014-11-03 14:23:59 -0700

    week 1 fin

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
+  root to: "pages#front"
   resources :videos do
     collection do
       post :search, to: "videos#search"
     end
   end
   resources :categories
+  get 'register', to: "users#new"
+  get 'sign_in', to: 'sessions#new'
+  get 'home', to: 'videos#index'
+  get 'sign_out', to: 'sessions#destroy'
+  
+  resources :users, only: [:create]
+  resources :sessions, only: [:create]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,13 @@
 
 comedy = Category.create(name: 'comedy')
 drama = Category.create(name: 'drama')
-reality = Category.create(name: 'reality')
-watched = Category.create(name: 'watched recently')
 
 Video.create(title: 'South Park', description: 'A comedy of messed up kids', small_cover_url: '/tmp/south_park.jpg', category: comedy)
 Video.create(title: 'Family Guy', description: 'Basically copying south park and Simpsons', small_cover_url: '/tmp/family_guy.jpg', category: comedy)
 Video.create(title: 'Futurama', description: 'An oddly attractive alien chick', small_cover_url: '/tmp/futurama.jpg', category: comedy)
 Video.create(title: 'Monk', description: 'A delusional mental patient', small_cover_url: '/tmp/monk.jpg', large_cover_url: '/tmp/monk_large.jpg', category: drama)
+Video.create(title: 'South Park', description: 'A comedy of messed up kids', small_cover_url: '/tmp/south_park.jpg', category: comedy)
+Video.create(title: 'Family Guy', description: 'Basically copying south park and Simpsons', small_cover_url: '/tmp/family_guy.jpg', category: comedy)
+Video.create(title: 'Futurama', description: 'An oddly attractive alien chick', small_cover_url: '/tmp/futurama.jpg', category: comedy)
+Video.create(title: 'Monk', description: 'A delusional mental patient', small_cover_url: '/tmp/monk.jpg', large_cover_url: '/tmp/monk_large.jpg', category: drama)
+Video.create(title: 'South Park', description: 'A comedy of messed up kids', small_cover_url: '/tmp/south_park.jpg', category: comedy)
